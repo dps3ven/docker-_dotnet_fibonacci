6101dd274ff6f94bb4e906f49f5bb790e394e30a
dps3ven <dpitpro@gmail.com>
2020-05-28 22:48:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+##### Description:
+```
+Displays first 100 numbers in fibonacci sequence
+```
+---
+Requirements
+```
+dotnet core 2.2
+```
+Usage
+```bash
+dotnet run
+dotnet publish -C Release
+
+```
+Containerization
+```bash
+docker build -t <custom_image_name>:<tag> .
+docker run --name <custom_app_name> <custom_iamge_name>:<tag>
+
+
---
+++ app/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace fibonacci
+{
+    class Program
+    {
+        // F{n}=F_{n-1}+F_{n-2},
+        // every number after the first two is the sum of the two preceding ones
+        static void Main(string[] args)
+        {
+            List<BigInteger> numList = new List<BigInteger> { 0, 1 };
+            foreach (BigInteger num in Fibonacci(numList))
+            {
+                Console.WriteLine(num);
+            }         
+        }
+
+        public static List<BigInteger> Fibonacci(List<BigInteger> sequence)
+        {
+                BigInteger lastNum = sequence.Last();
+                BigInteger secondLast = sequence.ElementAt(sequence.Count() - 2);
+                BigInteger nextNum = secondLast + lastNum;
+                sequence.Add(nextNum);
+                if (sequence.Count <= 100)
+                {
+                    Fibonacci(sequence);
+                }
+                return sequence;
+
+        }
+    }
+}
